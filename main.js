a637a4929e6dd0e27bc67b3cfdc3503db2d5a7fe
Colin Jaffe <balloonasaurus@gmail.com>
2020-02-13 09:50:10 -0500

    Adds introduction and numbers challenges and edits for clarity.

--- main.js
+++ main.js
@@ -1,62 +1,78 @@
-console.log('Challenge #1');
+// ##### Introduction #####
+//
+// The below assignment will require NO functions, except for the challenge banner function in number 1.
+//
+// Instead, our focus is going to be on console.logging the characters at certain indices of strings.
+//
 
+
+
+
+
+// 1.
+//
+// Write a function that takes in a paramter and prints the string below, with the given paramter place of the $ symbol.
+// 
+// ###### Challenge $ #####
+//
+// Call it before EVERY challenge below!
+//
+// Even though we'll USUALLY be calling this function with a number indicating the challenge we're on, THIS TIME call it with the parameter 'Banner Function Works!'. You should see the below result:
+//
+// ###### Challenge Banner Function Works! #####
+//
+//
+// 2.
+//
 // Console out the numbers from 1-20, but only if they're divisible by 3.
 
 // Use `if` and a modulus operation.
 
 
-console.log('Challenge #2');
-
-// Same problem as #1, but no `if`s, `mods`, or `but`s (and no, that last one is
-// not a thing). If you did it right, you should run the loop 6 times with 1
-// console log per loop. That's called... a clue!
+// 3.
+//
+// Same problem as #2, but no `if`s, `mods`, or `but`s (and no, `but` is not a JavaScript tool). There may be more ways to solve this problem without `if` or `%` (there usually are in JavaScript!), but if you did it the way we did, you should run the loop 6 times with console log per loop iteration.
 
 
-console.log('Challenge #3');
 
+// 4.
+//
 // Console out the numbers from 1-20. BACKWARDS.
 
-// (And, optionally, in heels.)
 
-// (That's a reference that only Olds make, and only if they're SUPER CULTURED.)
-
-// (No extra credit will be given for wearing heels during this challenge.)
-
-
-console.log('Challenge #4');
-
+// 5.
+//
 // For the string 'Boy howdy am I good at this!', console out the letters
 // individually.
 
 
-console.log('Challenge #5');
-
+// 6.
+//
 // For the string, 'And getting better every day.', starting with the second
 // character, print out every third character.
 
 
-console.log('Challenge #6');
-
+// 7. 
+//
 // For the string 'Am I the best?', console out the letters individually, AND
 // UPPERCASED.
 
 
-console.log('Challenge #7');
-
+// 8.
+//
 // For the string 'Whoa, I am the best!', console out the letters individually,
 // but also capitalize the letters in the third word.
 
 
-console.log('Challenge #8');
-
-
+// 9.
+//
 // For the string 'I am become death, destroyer of worlds.', console out the letters
 //  individually, but backwards. That is, the first console log should be
 // `.`, followed by `s`, followed by `d`, and so on.
 
 
-console.log('Challenge #9');
-
+// 10.
+//
 const names = 'Colin|Mesuara|Drew|In Seop|Emma|Ben|J|Ryan|Olgy|Mike|Chris|John';
 
 // Console out the individual letters of each person in class today, which are
@@ -67,3 +83,7 @@ const names = 'Colin|Mesuara|Drew|In Seop|Emma|Ben|J|Ryan|Olgy|Mike|Chris|John';
 
 // Don't print the pipes!
 
+
+// 11.
+//
+// There is no 11. Celebrate! You did it!
